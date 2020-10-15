73d4ad2c09f1c4348a033e4060b2db3ac24bf720
kbu715 <kbu715@gmail.com>
2020-10-15 22:48:42 +0900

    FOOTER

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,10 +2,10 @@ import {combineReducers} from 'redux'
 import {connectRouter} from 'connected-react-router'
 
 const createRootReducer = (history) => combineReducers({
-    router: connectRouter(history),
-})
+    router: connectRouter(history), //향후에 history를 사용한 connectRouter를 router를 통해 불러온다.
+})// reducer를 이런식으로 명명하여 사용한다.
 
 export default createRootReducer;
 
 
-//리듀서에 들어와서 상태값을 어떻게 관리해줄지 정해준다!!!
+//Reducer에 들어와서 상태값을 어떻게 관리해줄지 정해준다!!!
--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,3 @@
-const { routerMiddleware } = require("connected-react-router");
-const { compose } = require("redux");
-
 import {createStore, compose, applyMiddleware} from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import {createBrowserHistory} from 'history'
@@ -15,7 +12,7 @@ const sagaMiddleware = createSagaMiddleware()
 
 const initialState = {};
 
-const middlewares = [sagaMiddleware, routerMiddleware(history)];
+const middlewares = [sagaMiddleware, routerMiddleware(history)]; //향후에 리덕스에 관련된 미들웨어를 추가할 수 있다.
 const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__; 
 //devtools:크롬같은데서 리덕스로 개발을 할 때 어떻게 상태가 진행되는지 볼수 있게 해주는 도구
 
@@ -26,7 +23,7 @@ const store = createStore(
     createRootReducer(history),
     initialState, //웹의 모든 초기값을 담고 있다.
     composeEnhancer(applyMiddleware(...middlewares))
-)
-sagaMiddleware.run(rootSaga)
+)// 이 세개를 합쳐서 store를 만들어주세요...
+sagaMiddleware.run(rootSaga) //sage middleware를 작동해주세요...
 
 export default store;
