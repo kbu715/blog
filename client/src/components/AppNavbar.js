dd176318b9cad1781858a5e0101c72853e124c11
kbu715 <kbu715@gmail.com>
2020-10-17 22:17:00 +0900

    10/17 navbar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { Collapse, Container, Navbar, NavbarToggler } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Collapse, Container, Nav, Navbar, NavbarToggler } from "reactstrap";
+import { Link } from "react-router-dom";
 const AppNavbar = () => {
-    return (
-        <>
-        <Navbar>
-            <Container>
-                <Link to="/" className="text-white text-decoration-none">
-                    My Side Project Blog
-                </Link>
-                <NavbarToggler />
-                <Collapse>
-                </Collapse>
-            </Container>
-        </Navbar>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar color="dark" expand="lg" className="sticky-top">
+        <Container>
+          <Link to="/" className="text-white text-decoration-none">
+            My Side Project Blog
+          </Link>
+          <NavbarToggler />
+          <Collapse isOpen={true} navbar>
+            <Nav className="ml-auto d-felx justify-content-around" navbar> 
+              {true ? (
+                <h1 className="text-white">authLink</h1>
+              ) : (
+                <h1 className="text-white">guestLink</h1>
+              )}
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 export default AppNavbar;
+
+/* 
+ml-auto : margin-left:auto
+d-flex : display:flex-Box
+justify-content-around : space-around? 
+*/
