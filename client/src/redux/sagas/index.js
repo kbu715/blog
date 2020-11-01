588a530ad50f28956f1b077ea7bb6f8ac0908960
kbu715 <kbu715@gmail.com>
2020-11-01 16:05:34 +0900

    1101 PostCardList

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -2,10 +2,11 @@ import {all, fork} from 'redux-saga/effects'
 import axios from 'axios';
 import authSaga from './authSaga';
 import dotenv from 'dotenv';
+import postSaga from './postSaga';
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 export default function* rootSaga() { //function* : 여러값을 반환하는 최신 문법 함수
-    yield all([fork(authSaga)]);
+    yield all([fork(authSaga), fork(postSaga)]);
 }
--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -2,13 +2,30 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import AppNavbar from '../components/AppNavbar'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import { Container } from 'reactstrap'
+
+import PostCardList from './normalRoute/PostCardList';
+import PostWrite from './normalRoute/PostWrite';
+import PostDetail from './normalRoute/PostDetail';
+import CategoryResult from './normalRoute/CategoryResult';
+import Search from './normalRoute/Search';
 
 const MyRouter = () => {
     return (
         <>
             <AppNavbar />
             <Header />
-            <h1>Hello Body</h1>
+            <Container id="main-body">
+                <Switch>
+                    <Route path="/" exact component={PostCardList} />
+                    <Route path="/posts" exact component={PostWrite} />
+                    <Route path="/posts/:id" exact component={PostDetail} />
+                    <Route path="/posts/category/:categoryName" exact component={CategoryResult} />
+                    <Route path="/search/searchTerm" exact component={Search} />
+                    <Redirect from="*" to="/" />
+                </Switch>
+            </Container>
             <Footer />
         </>
     )
